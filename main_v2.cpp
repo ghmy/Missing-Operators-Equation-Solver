191007bb6c30dfc564b9ad4b4dcc1c108cecbb16
Mehmet Yeşilyurt <myslyrt@hotmail.com>
2018-11-21 20:02:34 +0300

    Add files via upload

--- main_v2.cpp
+++ main_v2.cpp
@@ -14,147 +14,11 @@ using namespace std;
 // 1*2345-6*7*8+9=2018
 // 12*34*5+67-89=2018
 
-static int latestPlusMinusIndex;
-static float latestPlusMinusResult;
+static int latestPlusMinusIndex;       // Latest + or - index in the operand string
+static float latestPlusMinusResult;    // Latest result up to the latest + or - index in the operand string
 
-float getWholeResult(bool reverse, int ops[])
-{
-    float nums[NUM + 1];
-    int index = 0;
-    int digits = 0;
-    int slide;
-    int i = 0;
-    int opsLength = NUM;
-    bool mulDivBegin; // is multiplication & division operations begin
-    float mul;
-    float div;
-    float res;
-
-    int lOps[opsLength]; // Local copy of ops
-
-    for(int j = 0; j < opsLength; ++j)
-    {
-        lOps[j] = ops[j];
-        if(latestPlusMinusIndex == -1 && (ops[j] == 1 || ops[j] == 2))
-            latestPlusMinusIndex = j;
-    }
-
-    if(reverse)
-        for(int j = NUM + 1; j > 0; --j)
-            nums[NUM + 1 - j] = j;
-    else
-        for(int j = 1; j <= NUM + 1; ++j)
-            nums[j - 1] = j;
-
-    // Step 1 - Concatenation Operation
-
-    while(i < opsLength)
-    {
-        if(lOps[i] == 0)
-        {
-            nums[index] = nums[index] * 10 + nums[i + 1];
-            ++digits;
-            if(i == opsLength - 1)
-                opsLength = opsLength - digits;
-        }
-        else
-        {
-            slide = index + digits;
-            if(digits != 0)
-            {
-                for(int j = slide; j < opsLength; ++j)
-                {
-                    lOps[j - digits] = lOps[j];
-                    nums[j - digits + 1] = nums[j + 1];
-                }
-                opsLength = opsLength - digits;
-                i = index;
-            }
-            index = i + 1;
-            digits = 0;
-        }
-        ++i;
-    }
-
-    // End of Step 1 - Concatenation
-
-    // Step 2 - Multiplication & Division Operations
-    while(true)
-    {
-        index = 0;
-        i = 0;
-        slide = 0;
-        mulDivBegin = false;
-
-        while(i < opsLength)
-        {
-            while((lOps[i] == 3 || lOps[i] == 4) && i < opsLength)
-            {
-                if(mulDivBegin == false)
-                {
-                    index = i;
-                    mulDivBegin = true;
-                }
-                ++i;
-                ++slide;
-            }
-
-            if(slide != 0) // First multiplication & division operations found
-                break;
-
-            mulDivBegin = false;
-            ++i;
-        }
-        if(slide == 0)  // There is not anymore multiplication/division operands
-            break;
-
-        // multiplication & division operations
-        // First multiply the multiplicants and multiply the dividents
-        // Then divide the multiplication of multiplicants to multiplication of dividents
-
-        i = 0;
-        mul = nums[index + i];
-        div = 1;
-
-        while(i < slide)
-        {
-            if(lOps[index + i] == 3)
-                mul *= nums[index + i + 1];
-            else
-                div *= nums[index + i + 1];
-            ++i;
-        }
-
-        nums[index] = (float)mul / (float)div;
-
-        for(int j = index; j < opsLength; ++j)
-        {
-            lOps[j] = lOps[j + slide];
-            nums[j + 1] = nums[j + slide + 1];
-        }
-        opsLength = opsLength - slide;
-
-    }
-
-    // End of Step 2 - multiplication & division operations
-
-    // Step 3 - addition & subtraction operations
-
-    res = nums[0];
-    latestPlusMinusResult = res;
-
-    for(int i = 0; i < opsLength; ++i)
-    {
-        if(lOps[i] == 1)
-            res += nums[i + 1];
-        else
-            res -= nums[i + 1];
-    }
-    return res;
-}
-
-// TODO implement this part
-float getPartialResult(bool reverse, int ops[])
+// Main function for calculating result of a particular operand string
+float getResult(bool reverse, int ops[])
 {
     float nums[NUM - latestPlusMinusIndex];
     int index = 0;
@@ -166,14 +30,23 @@ float getPartialResult(bool reverse, int ops[])
     float mul;
     float div;
     float res;
+    int reInitLatestPlusMinusIndex = -1;
 
     int lOps[opsLength]; // Local copy of ops
 
-    for(int j = 0; j < opsLength; ++j)
+    if(latestPlusMinusIndex == -1)
     {
-        lOps[j] = ops[j + latestPlusMinusIndex + 1];
-        //if(latestPlusMinusIndex == -1 && (ops[j] == 1 || ops[j] == 2))
-            //latestPlusMinusIndex = j;
+        for(int j = 0; j < opsLength; ++j)
+        {
+            lOps[j] = ops[j];
+            if(ops[j] == 1 || ops[j] == 2)
+                reInitLatestPlusMinusIndex = j;
+        }
+    }
+    else
+    {
+        for(int j = 0; j < opsLength; ++j)
+            lOps[j] = ops[j + latestPlusMinusIndex + 1];
     }
 
     if(reverse)
@@ -182,7 +55,6 @@ float getPartialResult(bool reverse, int ops[])
     else
         for(int j = 1; j <= NUM - latestPlusMinusIndex; ++j)
             nums[j - 1] = j + latestPlusMinusIndex + 1;
-
 
     // Step 1 - Concatenation Operation
 
@@ -278,38 +150,39 @@ float getPartialResult(bool reverse, int ops[])
 
     // Step 3 - addition & subtraction operations
 
-    res = latestPlusMinusResult;
-
-    if(ops[latestPlusMinusIndex] == 1)
-        res += nums[0];
-
-    else
-        res -= nums[0];
-
-    for(int i = 0; i < opsLength; ++i)
+    if(reInitLatestPlusMinusIndex != -1) // Calculating from beginning
     {
-        if(lOps[i] == 1)
-            res += nums[i + 1];
+        latestPlusMinusIndex = reInitLatestPlusMinusIndex;
+        res = nums[0];
+        for(int i = 0; i < opsLength; ++i)
+        {
+            latestPlusMinusResult = res;
+            if(lOps[i] == 1)
+                res += nums[i + 1];
+            else
+                res -= nums[i + 1];
+        }
+    }
+    else  // Calculation is done after the latest + or - result obtained in previous iterations
+    {
+        res = latestPlusMinusResult;
+        if(ops[latestPlusMinusIndex] == 1)
+            res += nums[0];
         else
-            res -= nums[i + 1];
+            res -= nums[0];
+
+        for(int i = 0; i < opsLength; ++i)
+        {
+            if(lOps[i] == 1)
+                res += nums[i + 1];
+            else
+                res -= nums[i + 1];
+        }
     }
     return res;
 }
 
-// TODO : implement combined function
-float getResult(bool reverse, int ops[])
-{
-    return 0;
-}
-
-bool getResult(bool reverse, int ops[], int result)
-{
-    if(latestPlusMinusIndex == -1)
-        return getWholeResult(reverse, ops) == result;
-    else
-        return getPartialResult(reverse, ops) == result;
-}
-
+// Make string to display on std
 string makeString(bool reverse, int ops[], int number)
 {
     string s = "";
@@ -339,6 +212,9 @@ string makeString(bool reverse, int ops[], int number)
     return s;
 }
 
+// All possible operands are put between every digit
+// Including concatenation
+// Possibilities are added from end to the beginning of the operand string
 list<string> solve(bool reverse, int result)
 {
     list<string> solutions;
@@ -356,7 +232,7 @@ list<string> solve(bool reverse, int result)
     {
         index = NUM - 1;
 
-        if( getResult(reverse, ops, result ))
+        if( getResult(reverse, ops) == result )
             solutions.push_back(makeString(reverse, ops, result));
 
         while(ops[index] == OPS - 1) --index;
@@ -383,7 +259,6 @@ list<string> solve(bool reverse, int result)
 
 int main()
 {
-
     int dest;
     cout << "Enter destination number : " << endl;
     cin >> dest;
